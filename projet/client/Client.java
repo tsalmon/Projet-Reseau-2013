5b1b440c0fc98d9ae8ea4f1645ac2280e83a005d
Thomas Salmon <th_s@hotmail.fr>
2013-05-21 22:47:25 +0200

    SHOPLIST: pas de reponses de vill

--- projet/client/Client.java
+++ projet/client/Client.java
@@ -1,7 +1,29 @@
+import java.net.*;
+import java.util.*;
+import java.io.*;
 public class Client
 {
+    static Scanner lecteur = new Scanner(System.in);
     public static void main(String [] args)
     {
-	
+	try{
+            DatagramSocket clientSocket = new DatagramSocket();
+            InetAddress IPAddress = InetAddress.getByName("127.0.0.1");
+            byte[] sendData = new byte[1024];
+            byte[] receiveData = new byte[1024];
+            String sentence = "SHOPLIST";//lecteur.next();
+            sendData = sentence.getBytes();
+            DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 9876);
+            clientSocket.send(sendPacket);
+            DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+            clientSocket.receive(receivePacket);
+            String modifiedSentence = new String(receivePacket.getData());
+            System.out.println("FROM SERVER:" + modifiedSentence);
+            clientSocket.close();
+        }
+        catch(Exception e)
+            {
+                e.printStackTrace();
+            }
     }
 }
